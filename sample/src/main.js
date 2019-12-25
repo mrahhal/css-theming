83f0b8a69131f04c8e66074c655b4d17e04e2400
Mohammad Rahhal <m.r992@hotmail.com>
2019-12-25 15:19:52 +0200

    sample: Hook up "t" shortcut to flip themes

--- sample/src/main.js
+++ sample/src/main.js
@@ -1,10 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+
+const htmlElement = document.getElementsByTagName('html')[0];
+
+let previousTheme = 'theme-default';
+htmlElement.classList.add(previousTheme);
+
+document.addEventListener('keypress', e => {
+  if (e.defaultPrevented) return;
+
+  if (e.key === 't') {
+    const newTheme = previousTheme == 'theme-default' ? 'theme-default-dark' : 'theme-default';
+    htmlElement.classList.remove(previousTheme);
+    htmlElement.classList.add(newTheme);
+    previousTheme = newTheme;
+  }
+});
