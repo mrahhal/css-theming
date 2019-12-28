4c433c1a383b9e3126452194b53e8a90382f6083
Mohammad Rahhal <m.r992@hotmail.com>
2019-12-28 11:58:25 +0200

    Create design-values.ts for querying design values

---
+++ src/design-values.ts
@@ -0,0 +1,45 @@
+var style = getComputedStyle(document.body);
+
+export function getDesignValue(prop: string) {
+	const value = style.getPropertyValue(prop);
+	if (value) {
+		return value.trim();
+	}
+
+	return null;
+}
+
+export function getDelimitedValue(prop: string) {
+  const value = getDesignValue(prop);
+  if (value) {
+    return value.split(',').map(x => x.trim());
+  }
+
+  return null;
+}
+
+let swatchNames: string[];
+let fgSwatchNames: string[];
+let colorNames: string[];
+let semanticColorNames: string[];
+let themeNames: string[];
+
+export function getSwatchNames() {
+	return swatchNames || (swatchNames = getDelimitedValue('--ct-swatch-names')!);
+}
+
+export function getFgSwatchNames() {
+	return fgSwatchNames || (fgSwatchNames = getDelimitedValue('--ct-fg-swatch-names')!);
+}
+
+export function getColorNames() {
+	return colorNames || (colorNames = getDelimitedValue('--ct-color-names')!);
+}
+
+export function getSemanticColorNames() {
+	return semanticColorNames || (semanticColorNames = getDelimitedValue('--ct-semantic-color-names')!);
+}
+
+export function getThemeNames() {
+	return themeNames || (themeNames = getDelimitedValue('--ct-theme-names')!);
+}
